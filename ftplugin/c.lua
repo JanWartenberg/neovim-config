f9649098ef08a0f2988bb54e7b2a5b31d6495332
Jan Wartenberg <jan.wartenberg@web.de>
2025-02-09 13:42:28 +0100

    C formatting adapted again

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,6 +1,7 @@
 -- tabs
+-- was 2 -- also according to "Google C++ Style Guide"
 vim.opt.expandtab = true -- insert spaces on tab
-vim.opt.tabstop = 2      -- existing tab: show 2 spaces
-vim.opt.softtabstop = 2  -- how many spaces the cursor moves
-vim.opt.shiftwidth = 2   -- when indenting with '>', use 2 spaces
+vim.opt.tabstop = 4      -- existing tab: show 2 spaces
+vim.opt.softtabstop = 4  -- how many spaces the cursor moves
+vim.opt.shiftwidth = 4   -- when indenting with '>', use 2 spaces
 
