ff20e7dd69ba76ebba327abebffe507880b83da6
Jan Wartenberg <jan.wartenberg@web.de>
2025-02-26 18:11:53 +0100

    Grep always from current buffers directory

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,8 +2,10 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    -- changes the working directory to the directory of the current file
+    vim.cmd("cd %:p:h")
+    -- alternative would be   :vimgrep /pattern/ **/*
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<C-h>', builtin.oldfiles, {})
-vim.keymap.set('n', '<C-q>', builtin.quickfix, {})  -- quickfixhistory would be an alternative
-
+vim.keymap.set('n', '<C-q>', builtin.quickfix, {}) -- quickfixhistory would be an alternative
