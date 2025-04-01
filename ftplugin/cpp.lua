299af4d726374e645a98d5a30bf2c02ed4a2b0d2
Z659785 <janphilipp.wartenberg@zf.com>
2025-04-01 11:01:41 +0200

    cpp indentation

---
+++ ftplugin/cpp.lua
@@ -0,0 +1,5 @@
+-- tabs
+vim.opt.expandtab = true -- insert spaces on tab
+vim.opt.tabstop = 2      -- existing tab: show 2 spaces
+vim.opt.softtabstop = 2  -- how many spaces the cursor moves
+vim.opt.shiftwidth = 2   -- when indenting with '>', use 2 spaces
